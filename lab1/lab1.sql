ee815aa883b8a1410718729f915dc10e177e842e
meaqqlee <magzhanmmm765@gmail.com>
2024-09-11 01:03:15 +0500

    Add lab1 SQL script

---
+++ lab1/lab1.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE lab1;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50)
+);
+
+ALTER TABLE users
+ADD COLUMN isadmin INT;
+
+ALTER TABLE users
+ALTER COLUMN isadmin TYPE BOOLEAN USING isadmin::BOOLEAN;
+
+ALTER TABLE users
+ALTER COLUMN isadmin SET DEFAULT FALSE;
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+DROP TABLE tasks;
+
+DROP DATABASE lab1;
